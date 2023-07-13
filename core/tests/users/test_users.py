8228f9a55e127fd41440ee1687fc14f773d8091f
radthenone <krzyzak.codes@gmail.com>
2023-07-13 11:33:28 +0200

    new test user detail

--- core/tests/users/test_users.py
+++ core/tests/users/test_users.py
@@ -11,3 +11,14 @@ class TestUsersClient:
             url, format="json", HTTP_AUTHORIZATION="Token " + get_or_create_token.key
         )
         assert response.status_code == 200
+
+    @pytest.mark.usefixtures("auth_client", "get_or_create_token", "create_user")
+    def test_user_detail(self, auth_client, get_or_create_token, create_user):
+        url = reverse("user-detail", kwargs={"pk": create_user.id})
+        response = auth_client.get(
+            url,
+            format="json",
+            HTTP_AUTHORIZATION="Token " + get_or_create_token.key,
+        )
+        assert response.data.get("email") == create_user.email
+        assert response.status_code == 200
